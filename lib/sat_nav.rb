8a9574f61bc1ba787f3db2d9364600d27c7042ae
James Hunter <james.hunter@me.com>
2014-05-08 12:49:33 +0100

    Added ability to judge single journeys

--- lib/sat_nav.rb
+++ lib/sat_nav.rb
@@ -1,3 +1,19 @@
 class SatNav
+  attr_reader :junctions
 
+  def initialize(number_of_junctions, instructions)
+    letters = ("A".."Z").first(number_of_junctions)
+
+    @junctions = letters.inject({}) do |junctions, name|
+      junction_instrcutions = instructions.select { |i| i[0] == name }
+      junctions.merge(name => Junction.new(name, junction_instrcutions))
+    end
+  end
+
+  def distance_of(journey)
+    start = @junctions[journey[0]]
+    destination = @junctions[journey[1]]
+
+    start.to(destination.name)
+  end
 end
--- spec/sat_nav_spec.rb
+++ spec/sat_nav_spec.rb
@@ -1,5 +1,17 @@
 require 'sat_nav'
 
 describe SatNav do
-  
+  let (:sat_nav) { SatNav.new(5, ["AB5", "BC4", "CD7", "DC8", "DE6", "AD5", "CE2", "EB3", "AE7"]) }
+
+  it "knows all the junctions" do
+    expect(sat_nav.junctions.keys).to eq(["A", "B", "C", "D", "E"])
+  end
+
+  it "knows the distance between A and B is 5" do
+    expect(sat_nav.distance_of("AB")).to eq(5)
+  end
+
+  it "knows the distance between B and C is 4" do
+    expect(sat_nav.distance_of("BC")).to eq(4)
+  end
 end
