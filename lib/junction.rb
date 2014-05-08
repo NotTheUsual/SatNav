486ad70d5a27ed04dd83afa75c80952af9513be0
James Hunter <james.hunter@me.com>
2014-05-08 11:56:55 +0100

    Junctions now know how far given destinations are

--- lib/junction.rb
+++ lib/junction.rb
@@ -1,7 +1,17 @@
 class Junction
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name, instructions = [])
     @name = name
+    @distances = {}
+    instructions.each do |instruction|
+      destination = instruction[1]
+      distance = instruction[2]
+      @distances[destination] = distance.to_i
+    end
+  end
+
+  def to(destination)
+    @distances[destination]
   end
 end
--- spec/junction_spec.rb
+++ spec/junction_spec.rb
@@ -1,11 +1,19 @@
 require 'junction'
 
 describe Junction do
-  context "(when initialised)" do
-    it "has a name" do
-      a = Junction.new("A")
-      expect(a.name).to eq("A")
-    end
+  it "has a name" do
+    a = Junction.new("A")
+    expect(a.name).to eq("A")
   end
   
+  it "knows the distance to another junction when seeded instructions" do
+    a = Junction.new("A", ["AB4"])
+    expect(a.to("B")).to eq(4)
+  end
+
+  it "knows the distance to multiple junctions" do
+    a = Junction.new("A", ["AB4", "AD3"])
+    expect(a.to("B")).to eq(4)
+    expect(a.to("D")).to eq(3)
+  end
 end
